68450d7355fefd8eb54417a6a0007faefb954bdd
Ay-dotcode <joseyowolabi@gmail.com>
2024-08-02 12:17:46 +0300

    just remaining the js

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 const active = document.getElementById('active');
 const noPeople = document.getElementById('noPeople');
+const billInput = document.getElementById('billInput');
+const peopleInput = document.getElementById('peopleInput');
+const resetBtn = document.getElementById('resetBtn');
 
 // noPeople.classList.replace('noError', 'error') // Add error id to noPeople
 
 // addActive(active); // making a buttn active
+resetBtn.addEventListener('click', () => {
+    peopleInput.value = '';
+    billInput.value = '';
+    resetBtn.disabled = true;
+    noPeople.classList.replace('error', 'noError');
+})
 
 function addActive(elem) {
     elem.style.backgroundColor = 'hsl(172, 67%, 45%)'
