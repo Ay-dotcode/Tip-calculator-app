abc37fc9ce257283a5ad72eb7a245ef71b2c85f5
Ay-dotcode <joseyowolabi@gmail.com>
2024-08-02 13:36:05 +0300

    done most of the js just rremaining to arrange it

--- app.js
+++ app.js
@@ -3,16 +3,46 @@ const noPeople = document.getElementById('noPeople');
 const billInput = document.getElementById('billInput');
 const peopleInput = document.getElementById('peopleInput');
 const resetBtn = document.getElementById('resetBtn');
+const errorMsg = document.getElementById('errorMsg');
 
-// noPeople.classList.replace('noError', 'error') // Add error id to noPeople
+resetBtn.disabled = true;
 
-// addActive(active); // making a buttn active
-resetBtn.addEventListener('click', () => {
-    peopleInput.value = '';
-    billInput.value = '';
-    resetBtn.disabled = true;
-    noPeople.classList.replace('error', 'noError');
-})
+billInput.addEventListener('input', isValid);
+peopleInput.addEventListener('input', isValid);
+
+function isValid() {
+    // if bill or people is invalid, reset button is disabled
+    if (peopleValid() || billValid())
+        resetBtn.disabled = true;
+    else resetBtn.disabled = false;
+}
+
+function peopleValid() {
+    if (peopleInput.value === '') {
+        noPeople.classList.replace('error', 'noError');
+        errorMsg.innerText = '';
+        return true;
+    } else if (peopleInput.value === '0') {
+        noPeople.classList.replace('noError', 'error');
+        errorMsg.innerText = `Can\'t be zero`;
+        return true;
+    } else if (peopleInput.value < 0 || peopleInput.value.startsWith('-')) {
+        noPeople.classList.replace('noError', 'error');
+        errorMsg.innerText = `Can\'t be negative`;
+        return true;
+    } else if (peopleInput.value > 0) {
+        noPeople.classList.replace('error', 'noError');
+        errorMsg.innerText = '';
+        return false;
+    }
+}
+
+function billValid() {
+    if (billInput.value === '' || billInput.value === '0' || billInput.value < 0 || billInput.value.startsWith('-'))
+        return true;
+    else if (billInput.value > 0)
+        return false;
+}
 
 function addActive(elem) {
     elem.style.backgroundColor = 'hsl(172, 67%, 45%)'
